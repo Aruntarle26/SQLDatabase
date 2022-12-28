b7c9f2778e5b84d28338eb0610aac81ca9e0ebe3
Aruntarle26 <114640286+Aruntarle26@users.noreply.github.com>
2022-12-28 12:25:18 +0530

    Add files via upload

---
+++ DQL(select).sql
@@ -0,0 +1,6 @@
+select *from employee
+select empid,empname,empadress from employee
+select* from employee where deptid=4
+select *from employee where empsalary<=60000
+select * from employee where empsalary between 60000 and 70000
+select *from employee where empage>26
