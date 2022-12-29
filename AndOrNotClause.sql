2145cd460bff6b28770fcd9538055daaa6176139
Aruntarle26 <114640286+Aruntarle26@users.noreply.github.com>
2022-12-29 12:49:36 +0530

    Add files via upload

---
+++ AndOrNotClause.sql
@@ -0,0 +1,9 @@
+select*from employee where empname='arun' and deptid=5
+select*from employee where empname='arun' or deptid=3
+
+select*from employee where empsalary>50000 and empage<26
+select*from employee where empsalary>50000 or empage<26
+select*from employee where  not deptid=4
+select*from employee where  not empage<26
+select*from employee where  not deptid>2
+
